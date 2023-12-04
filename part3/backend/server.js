7c70841149c3f25e43fc0727d5234e40578a730f
Ken Nguyen <ken.nguyen@contemi.com>
2023-12-04 22:03:24 +0100

    s

--- part3/backend/server.js
+++ part3/backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors')
 var morgan = require("morgan");
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
 };
 
 const app = express();
